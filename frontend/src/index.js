f4f7242f3c3c2f078cb50bf2ee5c97dbbc73d469
sarvesh1587 <hm2300439@gmail.com>
2026-05-28 03:43:20 +0530

    Fix: Add missing index.css and react-intersection-observer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,11 @@
-// frontend/src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { GoogleReCaptchaProvider } from "@google-recaptcha/react";
 import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GoogleReCaptchaProvider
-      type="v3"
-      siteKey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
-      language="en"
-    >
-      <App />
-    </GoogleReCaptchaProvider>
+    <App />
   </React.StrictMode>,
 );
